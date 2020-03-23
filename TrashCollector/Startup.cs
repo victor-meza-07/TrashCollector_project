cfe1de9078835d7bc7cf3700dbeea675fa51a3ac
victor-meza-07 <victord.meza@gmail.com>
2020-03-23 16:22:08 -0500

    Implemented Routing

--- TrashCollector/Startup.cs
+++ TrashCollector/Startup.cs
@@ -12,6 +12,9 @@ using TrashCollector.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using TrashCollector.ActionFilters;
 
 namespace TrashCollector
 {
@@ -35,8 +38,13 @@ namespace TrashCollector
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
-            
-            
+
+            services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+            });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
